13224c158f5cbdae02f8b029e5e83e8d5131a0e7
Kirill <37961322+kyusov@users.noreply.github.com>
2020-07-24 15:17:19 +0800

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,10 +3,4 @@ import rootReducer from './reducers'
 
 const store = createStore(rootReducer)
 
-console.log(store.getState())
-
-// Каждый раз при обновлении состояния - выводим его
-// Отметим, что subscribe() возвращает функцию для отмены регистрации слушателя
-const unsubscribe = store.subscribe(() => console.log(store.getState()))
-
 export default store
